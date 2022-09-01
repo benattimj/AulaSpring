b51d7563fd666011ace1ee8a27fa09697061f45d
Murilo Benatti <benatti1104@gmail.com>
2022-09-01 20:03:34 +0100

    Iniciando Projeto

---
+++ src/main/java/com/mbj/cursombj/CursombjApplication.java
@@ -0,0 +1,13 @@
+package com.mbj.cursombj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursombjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursombjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbj/cursombj/CursombjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbj.cursombj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursombjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
